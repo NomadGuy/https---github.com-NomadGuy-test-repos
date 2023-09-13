bbc4e89213f3efb0deb9c5020e5ecfa511bebe78
NomadGuy <valerijbilenko3@gmail.com>
2023-09-13 17:55:39 +0200

    Added functions and button

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let selected = document.querySelector('.phrase');
-selected.click(alert('Если чё, я всегда готов, хо на мне огонь!'))
+selected.addEventListener('click', callback)
+let button = document.querySelector('#myButton');
+button.addEventListener('click', amogus)
+
+function amogus() {
+    selected.innerHTML = 'У меня, всё всегда с собой, летом и зимой, и мне пох!'
+}
+function callback() {
+    alert('Если чё, я всегда готов, хо на мне огонь!')
+}
